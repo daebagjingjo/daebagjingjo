c43b6d9c7c963b34a8f29105a210f432afe17111
Wickhye <jihyesim170@gmail.com>
2022-12-09 20:49:29 +0900

    연결리스트 구현

--- test.c
+++ test.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+typedef int element;
+typedef struct Node{
+	element data;
+	struct Node* next;
+}Node;
 
-int main()
-{
-    
-    return 0;
+Node* insertFirst(Node* head, element data){
+	Node* node = (Node *)malloc(sizeof(Node));
+	node->data = data;
+	node->next = head;
+	head = node;
+	return head;
+}
+Node* deleteFirst(Node* head){
+	if (head == NULL) return NULL;
+	Node* removed = head;
+	Node* newhead = head -> next;
+	free(removed);
+	removed = NULL;
+   return newhead;
+}
+void printList(Node* head){
+	Node* p = head;
+	while(p!=NULL){
+		printf("%d->", p->data);
+		p = p->next;
+	}
+	printf("NULL\n");
+}
+int main(){
+	Node* head = NULL;
+	for(int i = 0; i<5; i++){
+		head = insertFirst(head, i);
+		printList(head);
+	}
+	for(int i = 0; i<5; i++){
+		head = deleteFirst(head);
+		printList(head);
+	}
+	return 0;
 }
